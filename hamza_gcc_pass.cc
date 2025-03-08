21120e2729f3038eeda8f84625a4c57bc9b53c46
hamzateli <hteli1@myseneca.ca>
2025-03-08 15:15:11 -0500

    Added necessary header files

---
+++ hamza_gcc_pass.cc
@@ -0,0 +1,13 @@
+/* This pass was creatd by Hamza Teli with the help of
+Professor Chris Tyler's SPO600 wiki and lecture. 
+
+This pass accomplishes the following:
+
+
+*/
+
+#include "gcc-plugin.h"
+#include "plugin-version.h"
+#include "cgraph.h" // As stated in SPO600 wiki
+#include "basic-block.h" // As stated in SPO600 wiki
+#include "gimple-iterator.h" // As stated in SPO600 wiki
